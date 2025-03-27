6673ba7cc3238604623e3f10b143d11bb5ddf6f1
Enzo Feltrin <enzobrexo@gmail.com>
2025-03-27 17:52:05 -0300

    formatando a página items.jsx

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,17 +1,14 @@
-import { useEffect, useState } from "react";
 import { useItemCollection } from "../hooks/useItemsCollection";
 import { Link } from "react-router-dom";
 
-
 export default function Items() {
   const { items } = useItemCollection();
-  
 
   return (
-    <>
+    <div className="stock-tables">
       <div className="items-table">
         <div className="table-highlight-items">
-          <h5>ID</h5>
+          <p>ID</p>
           <p>Nome</p>
           <p>Em Estoque</p>
           <p>Categoria</p>
@@ -21,20 +18,20 @@ export default function Items() {
           <div key={item.id} className="table-content-items">
             <p>{item.id}</p>
             <p>{item.name}</p>
-            <p>{item.stock}</p>
+            <p>{item.quantity}</p>
             <p>{item.category}</p>
-            <div className="actions">
+            <div className="options-container">
+              <Link>
+                <button>Visualizar</button>
+              </Link>
               <Link to={`/items/${item.id}`}>
                 <button>Editar</button>
               </Link>
-                <button>Excluir</button>
+              <button>Excluir</button>
             </div>
           </div>
         ))}
-
-
-        <div className="table-content"></div>
       </div>
-    </>
+    </div>
   );
 }
